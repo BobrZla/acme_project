34354fce9b9724183c57d97e4a9cf299e0468b7c
Dmitry <bobrzla@yandex.ru>
2024-02-28 15:39:37 +0300

    1

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
 
 
-def birthday(request):
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
     print(request.POST)
-    form = BirthdayForm(request.POST or None)
+    form = BirthdayForm(request.POST or None, instance=instance)
     context = {"form": form}
 
     if form.is_valid():
@@ -20,7 +24,17 @@ def birthday(request):
 def birthday_list(request):
     birthdays = Birthday.objects.all()
     context = {"birthdays": birthdays}
-    return render(request, 'birthday/birthday_list.html', context)
+    return render(request, "birthday/birthday_list.html", context)
+
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {"form": form}
+    if request.method == "POST":
+        instance.delete()
+        return redirect("birthday:list")
+    return render(request, "birthday/birthday.html", context)
     # if request.GET:
     #     form = BirthdayForm(request.GET)
     #     if form.is_valid():
